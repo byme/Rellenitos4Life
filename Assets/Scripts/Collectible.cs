5e819368a506c2a29d41e2662f1b03653ea1b2b4
JuanCaballeroF <caballerof204@gmail.com>
2023-06-30 21:17:02 -0500

    Particles effect with oil

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -6,7 +6,9 @@ public class Collectible : MonoBehaviour
 {
 
     public float pointsToAdd = 5f; // puntos a sumar
-    public LifeBar lifeBar; //referencia al script LifeBar 
+    public LifeBar lifeBar; //referencia al script LifeBar
+    public GameObject particlesEffect;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +17,8 @@ public class Collectible : MonoBehaviour
             // Aqui va la configuracion de lo que se quiera programar una vez el  jugador colisione con el colectable
             //Accede al componente LifeBar y aumenta los puntos 
             lifeBar.IncreaseLife(pointsToAdd);
+            GameObject obj = Instantiate(particlesEffect, transform.position, transform.rotation);
+            Destroy(obj, 1f);
 
             //Destruye objeto Colecionable 
             Destroy(gameObject);
