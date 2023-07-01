ea1d6c785804a3567d4dffadf271bd05a01939f4
JuanCaballeroF <caballerof204@gmail.com>
2023-06-30 20:46:01 -0500

    throwfood functionality

--- Assets/Scripts/ThrowFood.cs
+++ Assets/Scripts/ThrowFood.cs
@@ -8,16 +8,48 @@ public class ThrowFood : MonoBehaviour
     public GameObject target; // Target (player)
     public float throwingForce = 0.5f;
 
+
+    
+
+    // Movement speed in units per second.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
     private void Start()
     {
-        target = GameObject.Find("Square");
+        target = GameObject.Find("Player");
+
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(transform.position, target.transform.position);
 
     }
 
     void Update()
     {
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-        Vector2 direction = target.transform.position - transform.position;
-        rb.AddForce(direction*Time.deltaTime*throwingForce);
+
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, fractionOfJourney);
+        /*   Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+           Vector2 direction = target.transform.position - transform.position;
+           rb.AddForce(direction*throwingForce);*/
+
+        if (Mathf.Abs(transform.position.x - target.transform.position.x) < 0.20f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
